c6c97e3b476eee1fc3fb0d87f2c0484f328372e3
jackdoehrman <32639466+jackdoehrman@users.noreply.github.com>
2025-06-07 16:50:03 -0500

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">jack Doehrman.</h2>;
+  const three = <h3 className="big-heading">I analyze data.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a data analyst/scientist specializing in the sports industry, helping teams to grow their data fluency and reach. Currently, I’m focused on growing revenue and enhancing the event experience
+        <a href="https://houstondynamofc.com/" target="_blank" rel="noreferrer">
+          Houston Dynamo FC
         </a>
         .
       </p>
